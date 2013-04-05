230f36b13c3f07d65a0e96e70dd24b5412014de6
max <kharchenko.max@gmail.com>
2013-04-06 00:48:33 +0300

    Updated model.

--- src/main/java/org/springframework/webflow/samples/booking/User.java
+++ src/main/java/org/springframework/webflow/samples/booking/User.java
@@ -1,5 +1,7 @@
 package org.springframework.webflow.samples.booking;
 
+import org.hibernate.validator.constraints.NotBlank;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -13,6 +15,7 @@ import java.io.Serializable;
 public class User implements Serializable {
 
 	@Id
+	@NotBlank
     private String username;
 
     private String password;
