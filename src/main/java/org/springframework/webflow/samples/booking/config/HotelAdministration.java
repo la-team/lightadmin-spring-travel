2d38c78217fc9bd3e33437c79e50a7fe9e34526a
max-dev <kharchenko.max@gmail.com>
2013-10-28 01:25:42 +0200

    Changes applied according to LightAdmin public API

--- src/main/java/org/springframework/webflow/samples/booking/config/HotelAdministration.java
+++ src/main/java/org/springframework/webflow/samples/booking/config/HotelAdministration.java
@@ -1,64 +1,67 @@
 package org.springframework.webflow.samples.booking.config;
 
-import org.lightadmin.core.config.annotation.Administration;
-import org.lightadmin.core.config.domain.configuration.EntityMetadataConfigurationUnit;
-import org.lightadmin.core.config.domain.configuration.EntityMetadataConfigurationUnitBuilder;
-import org.lightadmin.core.config.domain.filter.FiltersConfigurationUnit;
-import org.lightadmin.core.config.domain.filter.FiltersConfigurationUnitBuilder;
-import org.lightadmin.core.config.domain.scope.DomainTypePredicate;
-import org.lightadmin.core.config.domain.scope.ScopesConfigurationUnit;
-import org.lightadmin.core.config.domain.scope.ScopesConfigurationUnitBuilder;
+import org.lightadmin.api.config.AdministrationConfiguration;
+import org.lightadmin.api.config.builder.EntityMetadataConfigurationUnitBuilder;
+import org.lightadmin.api.config.builder.FiltersConfigurationUnitBuilder;
+import org.lightadmin.api.config.builder.ScopesConfigurationUnitBuilder;
+import org.lightadmin.api.config.unit.EntityMetadataConfigurationUnit;
+import org.lightadmin.api.config.unit.FiltersConfigurationUnit;
+import org.lightadmin.api.config.unit.ScopesConfigurationUnit;
+import org.lightadmin.api.config.utils.DomainTypePredicate;
 import org.springframework.webflow.samples.booking.Hotel;
 
-import static org.lightadmin.core.config.domain.scope.ScopeMetadataUtils.all;
-import static org.lightadmin.core.config.domain.scope.ScopeMetadataUtils.filter;
+import static org.lightadmin.api.config.utils.ScopeMetadataUtils.all;
+import static org.lightadmin.api.config.utils.ScopeMetadataUtils.filter;
 
-@Administration( Hotel.class )
-public class HotelAdministration {
+@SuppressWarnings("unused")
+public class HotelAdministration extends AdministrationConfiguration<Hotel> {
 
-	public static EntityMetadataConfigurationUnit configuration( EntityMetadataConfigurationUnitBuilder configurationBuilder ) {
-		return configurationBuilder.nameField( "name" ).pluralName( "Hotels" ).build();
-	}
+    @Override
+    public EntityMetadataConfigurationUnit configuration(EntityMetadataConfigurationUnitBuilder configurationBuilder) {
+        return configurationBuilder.nameField("name").pluralName("Hotels").build();
+    }
 
-	public static FiltersConfigurationUnit filters( final FiltersConfigurationUnitBuilder filterBuilder ) {
-		return filterBuilder
-			.filter( "Name", "name" )
-			.filter( "Country", "country" )
-			.filter( "City", "city" ).build();
-	}
+    @Override
+    public FiltersConfigurationUnit filters(final FiltersConfigurationUnitBuilder filterBuilder) {
+        return filterBuilder
+                .filter("Name", "name")
+                .filter("Country", "country")
+                .filter("City", "city").build();
+    }
 
-	public static ScopesConfigurationUnit scopes( final ScopesConfigurationUnitBuilder scopeBuilder ) {
-		return scopeBuilder
-			.scope( "All", all() ).defaultScope()
-			.scope( "Low-cost", filter( lowCostHotels() ) )
-			.scope( "Regular", filter( regularHotels() ) )
-			.scope( "Luxury", filter( luxuryHotels() ) ).build();
-	}
+    @Override
+    public ScopesConfigurationUnit scopes(final ScopesConfigurationUnitBuilder scopeBuilder) {
+        return scopeBuilder
+                .scope("All", all()).defaultScope()
+                .scope("Low-cost", filter(lowCostHotels()))
+                .scope("Regular", filter(regularHotels()))
+                .scope("Luxury", filter(luxuryHotels())).build();
+    }
 
-	public static DomainTypePredicate<Hotel> lowCostHotels() {
-		return new DomainTypePredicate<Hotel>() {
-			@Override
-			public boolean apply( final Hotel hotel ) {
-				return hotel.getPrice().intValue() < 100 ;
-			}
-		};
-	}
+    private static DomainTypePredicate<Hotel> lowCostHotels() {
+        return new DomainTypePredicate<Hotel>() {
+            @Override
+            public boolean apply(final Hotel hotel) {
+                return hotel.getPrice().intValue() < 100;
+            }
+        };
+    }
 
-	public static DomainTypePredicate<Hotel> regularHotels() {
-		return new DomainTypePredicate<Hotel>() {
-			@Override
-			public boolean apply( final Hotel hotel ) {
-				return 100 <= hotel.getPrice().intValue() && hotel.getPrice().intValue() <= 250 ;
-			}
-		};
-	}
+    private static DomainTypePredicate<Hotel> regularHotels() {
+        return new DomainTypePredicate<Hotel>() {
+            @Override
+            public boolean apply(final Hotel hotel) {
+                return 100 <= hotel.getPrice().intValue() && hotel.getPrice().intValue() <= 250;
+            }
+        };
+    }
 
-	public static DomainTypePredicate<Hotel> luxuryHotels() {
-		return new DomainTypePredicate<Hotel>() {
-			@Override
-			public boolean apply( final Hotel hotel ) {
-				return hotel.getPrice().intValue() > 250;
-			}
-		};
-	}
+    private static DomainTypePredicate<Hotel> luxuryHotels() {
+        return new DomainTypePredicate<Hotel>() {
+            @Override
+            public boolean apply(final Hotel hotel) {
+                return hotel.getPrice().intValue() > 250;
+            }
+        };
+    }
 }
--- src/main/java/org/springframework/webflow/samples/booking/config/UserAdministration.java
+++ src/main/java/org/springframework/webflow/samples/booking/config/UserAdministration.java
@@ -1,22 +1,24 @@
 package org.springframework.webflow.samples.booking.config;
 
-import org.lightadmin.core.config.annotation.Administration;
-import org.lightadmin.core.config.domain.configuration.EntityMetadataConfigurationUnit;
-import org.lightadmin.core.config.domain.configuration.EntityMetadataConfigurationUnitBuilder;
-import org.lightadmin.core.config.domain.filter.FiltersConfigurationUnit;
-import org.lightadmin.core.config.domain.filter.FiltersConfigurationUnitBuilder;
+import org.lightadmin.api.config.AdministrationConfiguration;
+import org.lightadmin.api.config.builder.EntityMetadataConfigurationUnitBuilder;
+import org.lightadmin.api.config.builder.FiltersConfigurationUnitBuilder;
+import org.lightadmin.api.config.unit.EntityMetadataConfigurationUnit;
+import org.lightadmin.api.config.unit.FiltersConfigurationUnit;
 import org.springframework.webflow.samples.booking.User;
 
-@Administration( User.class )
-public class UserAdministration {
+@SuppressWarnings("unused")
+public class UserAdministration extends AdministrationConfiguration<User> {
 
-	public static EntityMetadataConfigurationUnit configuration( EntityMetadataConfigurationUnitBuilder configurationBuilder ) {
-		return configurationBuilder.nameField( "name" ).pluralName( "Customers" ).build();
-	}
+    @Override
+    public EntityMetadataConfigurationUnit configuration(EntityMetadataConfigurationUnitBuilder configurationBuilder) {
+        return configurationBuilder.nameField("name").pluralName("Customers").build();
+    }
 
-	public static FiltersConfigurationUnit filters( final FiltersConfigurationUnitBuilder filterBuilder ) {
-		return filterBuilder
-			.filter( "Name", "name" )
-			.filter( "Login", "username" ).build();
-	}
+    @Override
+    public FiltersConfigurationUnit filters(final FiltersConfigurationUnitBuilder filterBuilder) {
+        return filterBuilder
+                .filter("Name", "name")
+                .filter("Login", "username").build();
+    }
 }
